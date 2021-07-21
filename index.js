83dbebc9d6a7b09eba0bdd3b9094d1d4eb5672ef
ariful4082 <ariful4082@gmail.com>
2021-07-21 21:05:10 +0600

    included database name

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nine7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nine7.mongodb.net/emaJohnStore?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("emaJohnStore").collection("products");
